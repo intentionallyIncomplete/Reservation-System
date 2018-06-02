598d0e2562a479be05190982159d87ed49786ecf
Ian Bryan <ianbryanz@gmail.com>
2018-06-01 23:37:36 -0700

    Added Constructors & Commentary
    Added some constructors to the Table, ReservableItem classes. Added comments to class descriptions w/ formatting.

--- src/ReservationSystem/Table.java
+++ src/ReservationSystem/Table.java
@@ -1,5 +1,33 @@
 package ReservationSystem;
 
+import java.util.Scanner;
+
+/*
+ * <h4>Description:</h4>
+ * <p>This class is the Table class which creates an object which describes a table in 
+ * the restaurant. There is a count for the number of available seats at each table.
+ * <br>
+ * The class has a constructor which takes in a line from the text file "tableFile"
+ * 
+ * 
+ * @author Ian Bryan
+ * */
+
 public class Table extends ReservableItem{
 
+	/*Class variables*/
+	private int numberOfSeatsAvailable;
+	
+	/*
+	 * <p>This is the "default" constructor that takes in a line from the tableFile document.
+	 * The necessary information is extracted from the line and assigned to
+	 * a class variable for the number of seats available per table and provided to the super class.
+	 * </p>
+	 * 
+	 * @param Scanner inFile --> a line from the data file
+	 * */
+	public Table(Scanner inFile) {
+		
+	}
+	
 }
